0de54884034dec8de3efcb070178df7bd5096bd2
Tiago de Lima <tiago.blima02@gmail.com>
2022-08-31 17:24:44 -0300

    improving annotation conversion

--- convert_json.py
+++ convert_json.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 from typing import Literal
 import srsly
 from spacy.language import Language
@@ -10,23 +11,83 @@ import re
 nlp = spacy.blank('pt')
 
 
-def get_gold_token(text, start_char, tokens_delimiters=None):
+def get_gold_token(text, start_char, end_char, tokens_delimiters=None, token_alignment='expand'):
     if tokens_delimiters is None:
         tokens_delimiters = [' ', '\n', '\t']
+    doc = nlp.make_doc(text)
+    new_span = doc.char_span(*(start_char, end_char), alignment_mode=token_alignment)
 
-    limit_chars = text[:start_char]
-    gold_token = []
-    end_char = start_char
-    for char in reversed(limit_chars):
+    if new_span is None or new_span.start_char == new_span.end_char:
 
-        if char in tokens_delimiters and start_char != end_char:
-            break
-        else:
-            start_char -= 1
-            gold_token.append(char)
+        limit_chars = text[:start_char]
+        gold_token = []
+        end_char = start_char
 
-    gold_token = ''.join(reversed(gold_token))
-    return start_char, end_char, gold_token
+        for char in reversed(limit_chars):
+
+            if char in tokens_delimiters and start_char != end_char:
+                break
+            else:
+                start_char -= 1
+                gold_token.append(char)
+
+        new_span = doc.char_span(*(start_char, end_char), alignment_mode=token_alignment)
+        start_char, end_char = new_span.start_char, new_span.end_char
+    else:
+        start_char, end_char = new_span.start_char, new_span.end_char
+
+    return start_char, end_char
+
+
+pattern = re.compile(r'(?<=[a-z|A-z])[.,!?]')
+
+
+def find_token_span(text):
+    ents = []
+    matches = re.finditer(pattern, text)
+    for match in matches:
+        span_start, span_end = match.span()
+
+        non_puncted_text = re.sub('[.,!?]', ' ', text)
+        start_char, end_char = get_gold_token(non_puncted_text, span_start, span_end, tokens_delimiters=None)
+        if text[span_start:span_end] in ['.', '?', '!']:
+            ents.append((start_char, end_char, "PERIOD"))
+        elif text[span_start:span_end] in [',']:
+            ents.append((start_char, end_char, "COMMA"))
+
+    return ents
+
+
+def drop_duplicates(annotation):
+    new_annotation = []
+    texts_ids = []
+    for annot in annotation:
+        if annot["text_id"] not in texts_ids:
+            new_annotation.append(annot)
+            texts_ids.append(annot["text_id"])
+    return new_annotation
+
+
+def check_mergebility(annot, ents):
+    """
+    Check if the annotation is mergeable with the previous annotation.
+    :param ents: list of entities
+    :param annot: annotation to be checked
+    :return:  True if the annotation is mergeable with the previous annotation, False otherwise
+    """
+    merge = False
+    if len(ents) == 0:
+        return True
+    else:
+        for ent in ents:
+            if ent[1] > ent[0] > annot[1]:
+                merge = True
+            elif ent[0] < ent[1] < annot[0]:
+                merge = True
+            else:
+                merge = False
+                break
+    return merge
 
 
 def convert_annotations(
@@ -36,10 +97,16 @@ def convert_annotations(
     """Converte jsonl do doccano para o estilo de anotação do SpaCy e retorna um docbin com todos dos docs"""
     result = [p for p in pathlib.Path(path).glob('./**/Anotações')]
     result.sort()
+    error_map = {
+        'Esqueceu pontuação final [?!.]': 0,
+        'Trocou pontuação final por vírgula': 0,
+        'Esqueceu a vírgula': 0,
+        'Trocou a vírgula por ponto final.': 0
+    }
 
     annotator_entities = []
     student_entities = []
-
+    print(result)
     for week_path in result:
         print(week_path)
         overlaps = 0
@@ -50,73 +117,68 @@ def convert_annotations(
         annotated_jsonl = [tuple(srsly.read_jsonl(path)) for path in jsonls]
         annotated_pairs = tuple(zip(*annotated_jsonl))
 
-        for text_id, zipped_anot_data in enumerate(annotated_pairs, start=1):
+        for i, zipped_anot_data in enumerate(annotated_pairs, start=1):
 
-            # verifica se os textos entre os anotadores são iguais
-            assert all(zipped_anot_data[0]['text'] == d['text'] for d in zipped_anot_data), \
-                'A sequencia de textos está diferente. por tanto não é possível comparar'
+            text = zipped_anot_data[0]['text']
+            text_id = zipped_anot_data[0]['id']
+            student_entity = {'text': text, 'text_id': text_id, 'ents': []}
 
-            # cria o documento pelo texto do primeiro anotador
+            annotator_entity = defaultdict(lambda: {'text': text, 'text_id': text_id, 'ents': []})
             doc = nlp.make_doc(zipped_anot_data[0]['text'])
-            # doc.user_data = {'student': '...'} # é possível adicionar informações dos alunos ao dataset
-            text = zipped_anot_data[0]['text'].replace('\n', ' ').replace('\t', ' ')
+            # Procura pela pontuação do aluno no texto
 
-            end_chars = []
-            student_entity = {
-                'text': text,
-                'sentence_id': text_id,
-                'ents': []
-            }
-            annotator_entity = {
-                'text': text,
-                'sentence_id': text_id,
-                'ents': []
-            }
+            student_entity["ents"] = find_token_span(text)
+            student_entities.append(student_entity)
 
             for annotator_id, annotation in enumerate(zipped_anot_data, start=1):
-                # criando spans a partir dos indices
-                errors_filter = ['Erro de Pontuação', 'Erro de Vírgula']
 
                 for s in annotation['label']:
 
-                    if len(annotation['label']) > 2 and s[2] in errors_filter:
-                        new_span = doc.char_span(*s, alignment_mode=token_alignment)
+                    if s[2] == 'Erro de Pontuação':
+                        start_char, end_char = get_gold_token(text, s[0], s[1])
+                        if text[s[0]:s[1]] != '.':
+                            ent_span = (start_char, end_char, "PERIOD")
+                            ann_ents = annotator_entity[annotator_id]["ents"]
 
-                        if new_span is None or new_span.start_char == new_span.end_char:
-                            start_char, end_char, gold_token = get_gold_token(text, s[1])
-                            new_span = doc.char_span(*(start_char, end_char), alignment_mode=token_alignment)
+                            if check_mergebility(ent_span, ann_ents):
+                                annotator_entity[annotator_id]["ents"].append(ent_span)
 
-                        if all(start < new_span.start_char for start in end_chars):
-                            end_chars.append(new_span.end_char)
+                            if text[s[0]:s[1]] == ',':
+                                error_map['Trocou pontuação final por vírgula'] += 1
+                            else:
+                                error_map['Esqueceu pontuação final [?!.]'] += 1
 
+                    elif s[2] == 'Erro de vírgula':
+                        start_char, end_char = get_gold_token(text, s[0], s[1])
+                        ent_span = (start_char, end_char, "COMMA")
+                        ann_ents = annotator_entity[annotator_id]["ents"]
+
+                        if check_mergebility(ent_span, ann_ents):
+                            annotator_entity[annotator_id]["ents"].append(ent_span)
                             if text[s[0]:s[1]] == '.':
-                                student_entity['ents'].append(
-                                    {"start": new_span.start_char, "end": new_span.end_char, "label": "PERIOD"})
-                            elif text[s[0]:s[1]] == ',':
-                                student_entity['ents'].append(
-                                    {"start": new_span.start_char, "end": new_span.end_char, "label": "COMMA"})
+                                error_map['Trocou a vírgula por ponto final.'] += 1
+                            else:
+                                error_map['Esqueceu a vírgula'] += 1
 
-                            if s[2] == 'Erro de Pontuação':
-                                if text[s[0]:s[1]] != '.':
-                                    annotator_entity['ents'].append(
-                                        {"start": new_span.start_char, "end": new_span.end_char, "label": "PERIOD"})
-                            elif s[2] == 'Erro de Vírgula':
-                                if text[s[0]:s[1]] != ',':
-                                    student_entity['ents'].append(
-                                        {"start": new_span.start_char, "end": new_span.end_char, "label": "COMMA"})
-                                else:
-                                    student_entity['ents'].append(
-                                        {"start": new_span.start_char, "end": new_span.end_char, "label": "I-COMMA"})
-                        else:
+                ann_ents = annotator_entity[annotator_id]["ents"]
 
-                            overlaps += 1
-            student_entities.append(student_entity)
+                for sts_ents in student_entity["ents"]:
+
+                    if check_mergebility(sts_ents, ann_ents):
+                        annotator_entity[annotator_id]["ents"].append(sts_ents)
+
             annotator_entities.append(annotator_entity)
-        print("overlaps: ", overlaps)
     return student_entities, annotator_entities
 
 
 if __name__ == '__main__':
     sts_entities, annot_entities = convert_annotations('Anotation/')
+    annotator1 = list(map(lambda dict_annot: dict_annot[1], annot_entities))
+    annotator2 = list(map(lambda dict_annot: dict_annot[2], annot_entities))
+    print(len(annotator1), len(annotator2), len(sts_entities))
+    # annotator1 = drop_duplicates(annotator1)
+    # annotator2 = drop_duplicates(annotator2)
+    print(len(annotator1), len(annotator2), len(sts_entities))
     json.dump(obj=sts_entities, fp=open('student_entities.json', 'w'), indent=4)
-    json.dump(obj=annot_entities, fp=open('annotator_entities.json', 'w'), indent=4)
+    json.dump(obj=annotator1, fp=open('annotator1_entities.json', 'w'), indent=4)
+    json.dump(obj=annotator2, fp=open('annotator2_entities.json', 'w'), indent=4)
---
+++ punctuator/utils.py
@@ -0,0 +1,49 @@
+from nltk.tokenize import regexp
+from simpletransformers.ner import NERModel, NERArgs
+import torch
+
+
+def replace(sentence):
+    tokenizer = regexp.RegexpTokenizer(r'\w+|[.,?]')
+
+    tokens = tokenizer.tokenize(sentence.lower())
+
+    labels = []
+    for i, token in enumerate(tokens):
+        try:
+            if token not in ['.', ',', '?']:
+                # sent_data.append([sent_id,'O',token])
+                labels.append('O')
+            elif token in ['.', '?']:
+                # sent_data[-1][1] = 'PERIOD'
+                labels[-1] = 'PERIOD'
+            elif token == ',':
+                # sent_data[-1][1] = 'COMMA'
+                labels[-1] = 'COMMA'
+
+        except IndexError:
+            continue
+
+    return labels
+
+
+def load_model(path: str, labels: list, max_length: int = 512, model_type: str = 'bert'):
+    model_args = NERArgs()
+    model_args.labels_list = labels
+
+    model_args.max_seq_length = max_length
+    return NERModel(
+        model_type,
+        path,
+        args=model_args,
+        use_cuda=torch.cuda.is_available()
+    )
+
+
+def get_predicted_labels(model, sentence: str):
+    predicted_labels = model.predict([sentence])[0]
+
+    y_pred = []
+    for i, pred in predicted_labels:
+        y_pred.append(list(map(lambda item: list(item.values())[0], pred)))
+    return y_pred
