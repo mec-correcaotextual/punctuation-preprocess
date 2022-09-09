22ce33f60c121b6390a96a31753d37cc646bf090
Tiago de Lima <tiago.blima02@gmail.com>
2022-09-09 11:40:13 -0300

    convert to spacy entities

--- convert/convert_to_spacy_ents.py
+++ convert/convert_to_spacy_ents.py
@@ -1,94 +1,12 @@
 import json
+import pathlib
 from collections import defaultdict
 from typing import Literal
+
 import srsly
-from spacy.language import Language
-from spacy import tokens
-import spacy
-import pathlib
-import re
 
-nlp = spacy.blank('pt')
-
-
-def get_gold_token(text, start_char, end_char, tokens_delimiters=None, token_alignment='expand'):
-    if tokens_delimiters is None:
-        tokens_delimiters = [' ', '\n', '\t']
-    doc = nlp.make_doc(text)
-    new_span = doc.char_span(*(start_char, end_char), alignment_mode=token_alignment)
-
-    if new_span is None or new_span.start_char == new_span.end_char:
-
-        limit_chars = text[:start_char]
-        gold_token = []
-        end_char = start_char
-
-        for char in reversed(limit_chars):
-
-            if char in tokens_delimiters and start_char != end_char:
-                break
-            else:
-                start_char -= 1
-                gold_token.append(char)
-
-        new_span = doc.char_span(*(start_char, end_char), alignment_mode=token_alignment)
-        start_char, end_char = new_span.start_char, new_span.end_char
-    else:
-        start_char, end_char = new_span.start_char, new_span.end_char
-
-    return start_char, end_char
-
-
-pattern = re.compile(r'(?<=[a-z|A-z])[.,!?]')
-
-
-def find_token_span(text, token_alignment='expand'):
-    ents = []
-    matches = re.finditer(pattern, text)
-    for match in matches:
-        span_start, span_end = match.span()
-
-        non_puncted_text = re.sub('[.,!?]', ' ', text)
-        start_char, end_char = get_gold_token(non_puncted_text, span_start, span_end,
-                                              tokens_delimiters=None, token_alignment=token_alignment)
-        if text[span_start:span_end] in ['.', '?', '!']:
-            ents.append((start_char, end_char, "PERIOD"))
-        elif text[span_start:span_end] in [',']:
-            ents.append((start_char, end_char, "COMMA"))
-
-    return ents
-
-
-def drop_duplicates(annotation):
-    new_annotation = []
-    texts_ids = []
-    for annot in annotation:
-        if annot["text_id"] not in texts_ids:
-            new_annotation.append(annot)
-            texts_ids.append(annot["text_id"])
-    return new_annotation
-
-
-def check_mergebility(annot, ents):
-    """
-    Check if the annotation is mergeable with the previous annotation.
-    :param ents: list of entities
-    :param annot: annotation to be checked
-    :return:  True if the annotation is mergeable with the previous annotation, False otherwise
-    """
-    merge = False
-    if len(ents) == 0:
-        return True
-    else:
-        for ent in ents:
-            if ent[1] > ent[0] > annot[1]:
-                merge = True
-            elif ent[0] < ent[1] < annot[0]:
-                merge = True
-            else:
-                merge = False
-                break
-    return merge
+from convert_text import nlp, get_gold_token
+from utils import find_token_span, check_mergebility
 
 
 def convert_annotations(
@@ -173,10 +91,10 @@ def convert_annotations(
 
 
 if __name__ == '__main__':
-    sts_entities, annot_entities = convert_annotations('Anotation/')
+    sts_entities, annot_entities = convert_annotations('../Anotation/')
     annotator1 = list(map(lambda dict_annot: dict_annot[1], annot_entities))
     annotator2 = list(map(lambda dict_annot: dict_annot[2], annot_entities))
 
-    json.dump(obj=sts_entities, fp=open('annotations/student_entities.json', 'w'), indent=4)
-    json.dump(obj=annotator1, fp=open('annotations/annotator1_entities.json', 'w'), indent=4)
-    json.dump(obj=annotator2, fp=open('annotations/annotator2_entities.json', 'w'), indent=4)
+    json.dump(obj=sts_entities, fp=open('../annotations/student_entities.json', 'w'), indent=4)
+    json.dump(obj=annotator1, fp=open('../annotations/annotator1_entities.json', 'w'), indent=4)
+    json.dump(obj=annotator2, fp=open('../annotations/annotator2_entities.json', 'w'), indent=4)
